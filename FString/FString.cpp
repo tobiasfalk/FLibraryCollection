794b952444f0e82a828d0fe68a6af915c29c504c
tobias@hobbes <tobiasfalk200242@gmail.com>
2020-10-24 14:34:44 +0200

    FString start(FLC V1.6.0) Signed-off-by: tobias@hobbes <tobiasfalk200242@gmail.com>

--- FString/FString.cpp
+++ FString/FString.cpp
@@ -9,3 +9,83 @@ FString::string::~string()
 {
 
 }
+
+FString::string &FString::string::operator=(std::string str)
+{
+    this->assign(str);
+    return *this;
+}
+
+std::vector<FString::string> FString::string::split(string str, const char splitter)
+{
+    std::vector<string> strc;
+    for(unsigned int i = 0; i < str.numberof(splitter); i++)
+    {
+        string strt = str;
+        strt.erase((strt.find_first_of(splitter) - 1), (strt.length() - strt.find_first_of(splitter)));
+        strc.push_back(strt);
+        str.erase(0, str.find_first_of(splitter));
+    }
+    strc.push_back(str);
+    return strc;
+}
+
+std::vector<FString::string> FString::string::split(const char splitter)
+{
+    std::vector<string> strc;
+    string strt1 = *this;
+    for(unsigned int i = 0; i < numberof(splitter); i++)
+    {
+        string strt2 = strt1;
+        strt2.erase((strt2.find_first_of(splitter)), (strt2.length() - strt2.find_first_of(splitter)));
+        strc.push_back(strt2);
+        strt1.erase(0, strt1.find_first_of(splitter) + 1);
+    }
+    strc.push_back(strt1);
+    return strc;
+}
+
+bool FString::string::exists(string str, const char character)
+{
+    std::string::size_type n = 0;
+    n = str.find(character);
+    if (n == std::string::npos) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool FString::string::exists(const char character)
+{
+    std::string::size_type n = 0;
+    n = find(character);
+    if (n == std::string::npos) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+unsigned int FString::string::numberof(string str, const char character)
+{
+    unsigned int number = 0;
+    while (exists(str, character))
+    {
+        str.erase(str.find_first_of(character), 1);
+        number++;
+    }
+    return number;
+}
+
+unsigned int FString::string::numberof(const char character)
+{
+    string str = *this;
+    unsigned int number = 0;
+    while (exists(str, character))
+    {
+        str.erase(str.find_first_of(character), 1);
+        number++;
+    }
+    return number;
+}
--- FString/FString.hpp
+++ FString/FString.hpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <string>
 
+#include <vector>
+#include <list>
+
 ///
 /// The FString namespace is where all the FString things are
 /// it inherits all from the std::string
@@ -44,6 +47,48 @@ public:
     /// \return
     ///
     string& operator=(std::string str);
+
+    ///
+    /// \brief split splits the given string in to an vector of strings
+    /// \param str th given string
+    /// \param splitter the character where it will be splited(it will be removed
+    /// \return std::vector<string>
+    ///
+    std::vector<string> split(string str, const char splitter);
+    ///
+    /// \brief split splits string in to an vector of strings
+    /// \param splitter the character where it will be splited(it will be removed
+    /// \return std::vector<string>
+    ///
+    std::vector<string> split(const char splitter);
+
+    ///
+    /// \brief exists returns true when the given charector exists in the given string
+    /// \param str the given string
+    /// \param character the given character
+    /// \return boolean
+    ///
+    bool exists(string str, const char character);
+    ///
+    /// \brief exists returns true when the given charector exists in the string
+    /// \param character the given character
+    /// \return boolean
+    ///
+    bool exists(const char character);
+
+    ///
+    /// \brief numberof returns how often the given character exists in the given string
+    /// \param str
+    /// \param character
+    /// \return unsigned int
+    ///
+    unsigned int numberof(string str, const char character);
+    ///
+    /// \brief numberof returns how often the given character exists in the string
+    /// \param character
+    /// \return unsigned int
+    ///
+    unsigned int numberof(const char character);
 
 private:
 };
